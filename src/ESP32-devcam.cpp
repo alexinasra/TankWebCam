0fbddb5de325cd1fdc650fe23d767f31005ea84b
Alexi Nasra <alexinasra@gmail.com>
2019-11-22 22:34:37 +0200

    Initial commit

---
+++ src/ESP32-devcam.cpp
@@ -0,0 +1,162 @@
+#include <Arduino.h>
+#include "OV2640.h"
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+#include <AutoWifi.h>
+
+#include "SimStreamer.h"
+#include "OV2640Streamer.h"
+#include "OV2640.h"
+#include "CRtspSession.h"
+
+#define CAMERA_MODEL_AI_THINKER
+#define USEBOARD_AITHINKER
+
+
+#ifndef USEBOARD_AITHINKER
+// If your board has a GPIO which is attached to a button, uncomment the following line
+// and adjust the GPIO number as needed.  If that button is held down during boot the device
+// will factory reset.
+#define FACTORYRESET_BUTTON 32
+#endif
+
+
+const char* ssid = "ESPsoftAP_01";
+const char* password = "pass-to-soft-AP";
+
+OV2640 cam;
+
+WebServer server(80);
+
+WiFiServer rtspServer(8554);
+
+void handle_jpg_stream(void)
+{
+    WiFiClient client = server.client();
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+    server.sendContent(response);
+
+    while (1)
+    {
+        cam.run();
+        if (!client.connected())
+            break;
+        response = "--frame\r\n";
+        response += "Content-Type: image/jpeg\r\n\r\n";
+        server.sendContent(response);
+
+        client.write((char *)cam.getfb(), cam.getSize());
+        server.sendContent("\r\n");
+        if (!client.connected())
+            break;
+    }
+}
+
+void handle_jpg(void)
+{
+    WiFiClient client = server.client();
+
+    cam.run();
+    if (!client.connected())
+    {
+        return;
+    }
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-disposition: inline; filename=capture.jpg\r\n";
+    response += "Content-type: image/jpeg\r\n\r\n";
+    server.sendContent(response);
+    client.write((char *)cam.getfb(), cam.getSize());
+}
+
+void handleNotFound()
+{
+    String message = "Server is running!\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    server.send(200, "text/plain", message);
+}
+
+
+void setup()
+{
+
+    Serial.begin(115200);
+    while (!Serial)
+    {
+        ;
+    }
+
+    int camInit = cam.init(esp32cam_aithinker_config);
+    Serial.printf("Camera init returned %d\n", camInit);
+
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    server.on("/", HTTP_GET, handle_jpg_stream);
+    server.on("/jpg", HTTP_GET, handle_jpg);
+    server.onNotFound(handleNotFound);
+    server.begin();
+
+
+    rtspServer.begin();
+}
+
+CStreamer *streamer;
+CRtspSession *session;
+WiFiClient client; // FIXME, support multiple clients
+
+void loop()
+{
+    server.handleClient();
+    uint32_t msecPerFrame = 100;
+    static uint32_t lastimage = millis();
+
+    // If we have an active client connection, just service that until gone
+    // (FIXME - support multiple simultaneous clients)
+    if(session) {
+        session->handleRequests(0); // we don't use a timeout here,
+        // instead we send only if we have new enough frames
+
+        uint32_t now = millis();
+        if(now > lastimage + msecPerFrame || now < lastimage) { // handle clock rollover
+            session->broadcastCurrentFrame(now);
+            lastimage = now;
+
+            // check if we are overrunning our max frame rate
+            now = millis();
+            if(now > lastimage + msecPerFrame)
+                printf("warning exceeding max frame rate of %d ms\n", now - lastimage);
+        }
+
+        if(session->m_stopped) {
+            delete session;
+            delete streamer;
+            session = NULL;
+            streamer = NULL;
+        }
+    }
+    else {
+        client = rtspServer.accept();
+
+        if(client) {
+            //streamer = new SimStreamer(&client, true);             // our streamer for UDP/TCP based RTP transport
+            streamer = new OV2640Streamer(&client, cam);             // our streamer for UDP/TCP based RTP transport
+
+            session = new CRtspSession(&client, streamer); // our threads RTSP session and state
+        }
+    }
+}
